5dab3436841321184ff1e0913e025d370491cce3
unknown <Ivan>
2023-04-25 16:31:12 +0700

    done example_29

--- example_29/Program.cs
+++ example_29/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+Console.Write("Введите длинну массива --> ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = RandomArray(size);
+PrintArray(arr);
+
+int[] RandomArray(int length)
+{
+    int[] array = new int [length];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
