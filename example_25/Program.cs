52c2fd4279f49d6c2917370197e12db23acd3a6f
unknown <Ivan>
2023-04-25 15:54:58 +0700

    done example_25

--- example_25/Program.cs
+++ example_25/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("- Введите числа А и В -");
+Console.Write("Число А --> ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число В --> ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int degreeNumber = DegreeNumber(numberA, numberB);
+Console.WriteLine($"A в натуральной степени B = {degreeNumber}");
+
+int DegreeNumber(int numA, int numB)
+{
+    int num = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+      num *= numA; 
+    }
+    return num;
+}
